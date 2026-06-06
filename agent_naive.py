2c39153ca5f655ce7be2abbf4b2d82c139a222e4
akshay-r13 <akshay.raman97@gmail.com>
2026-06-06 10:30:06 +0530

    setting up

--- agent_naive.py
+++ agent_naive.py
@@ -12,8 +12,42 @@ A langgraph customer support agent with no context engineering principles applie
 """
 
 from pathlib import Path
+from langgraph.prebuilt import create_react_agent
+from langchain_core.tools import tool
 
 DATA_PATH = Path(__file__).parent / "data"
 DB_PATH = DATA_PATH / "ecommerce.db"
 POLICY_DOCUMENT_PATH = DATA_PATH / "policy_document.txt"
 
+@tool
+def lookup_order(order_id: str):
+    """Look up order from Ecomm Database based on ID"""
+    pass
+
+@tool
+def find_product(query: str):
+    """Find Products from Ecomm Database"""
+    pass
+
+@tool
+def read_policy(query: str):
+    """Read policy document"""
+    pass
+
+
+AGENT_SYSTEM_PROMPT = """
+You are a helpful AI assistant.
+Answer user questions concisely.
+"""
+
+agent = create_react_agent(
+    model="gemini-2.5-flash",
+    tools = [],
+    prompt = AGENT_SYSTEM_PROMPT
+)
+
+agent.invoke({
+    "messages": [
+        "Hey can you help me?"
+    ]
+})
